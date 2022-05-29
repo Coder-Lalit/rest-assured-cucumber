ef1d1f80253582f4af8768cd4233f90f70f8ce3e
Coder-Lalit <coder.lalit@gmail.com>
2022-05-29 20:08:00 +0530

    removing getter and setter

--- src/test/java/org/petstore/bean/Pet.java
+++ src/test/java/org/petstore/bean/Pet.java
@@ -3,15 +3,11 @@ package org.petstore.bean;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import lombok.experimental.Tolerate;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Getter
-@Setter
 @Data
 @Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
